c5686cd89720c8ae12d285a5747bb5dc51ee2edc
AruSeito <41471697+AruSeito@users.noreply.github.com>
2024-03-19 20:07:37 +0800

    feat: ✨ add userInfo dropdown

--- apps/agent/src/Layout/Workspace/pc/components/UserInfoContent/index.tsx
+++ apps/agent/src/Layout/Workspace/pc/components/UserInfoContent/index.tsx
@@ -1,8 +1,12 @@
-import { Avatar } from "antd"
-import { FC } from "react"
+import Icon from "@ant-design/icons"
+import { Avatar, Dropdown } from "antd"
+import { FC, forwardRef } from "react"
 import { useSelector } from "react-redux"
-import { useNavigate, useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { getCurrentUser } from "@illa-public/user-data"
+import DiscordIcon from "@/assets/public/discord.svg?react"
+import DocumentIcon from "@/assets/public/document.svg?react"
+import SettingIcon from "@/assets/public/setting.svg?react"
 import { getSettingPath } from "@/utils/routeHelper"
 import {
   avatarContainerStyle,
@@ -12,26 +16,68 @@ import {
   userInfoContentContainerStyle,
 } from "./style"
 
-const UserInfoContent: FC = () => {
+const UserInfoContent: FC = forwardRef<HTMLDivElement>((_props, ref) => {
   const userInfo = useSelector(getCurrentUser)
+
   const { teamIdentifier } = useParams()
-  const navigate = useNavigate()
+
   const toSettingPage = () => {
-    if (!teamIdentifier) return
+    if (!teamIdentifier) return ""
     const url = getSettingPath(teamIdentifier)
-    navigate(url)
+    return url
   }
+
+  const menuItems = [
+    {
+      key: "1",
+      icon: <Icon component={DiscordIcon} />,
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="https://discord.com">
+          Join our Discord
+        </a>
+      ),
+    },
+    {
+      key: "2",
+      icon: <Icon component={DocumentIcon} />,
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.illa.com"
+        >
+          Documentation
+        </a>
+      ),
+    },
+    {
+      key: "3",
+      icon: <Icon component={SettingIcon} />,
+      label: <Link to={toSettingPage()}>Setting</Link>,
+    },
+  ]
+
   return (
-    <div css={userInfoContentContainerStyle} onClick={toSettingPage}>
-      <div css={avatarContainerStyle}>
-        <Avatar src={userInfo.avatar} shape="circle" size={32} />
+    <Dropdown
+      arrow={false}
+      menu={{
+        items: menuItems,
+      }}
+      trigger={["click"]}
+    >
+      <div css={userInfoContentContainerStyle} ref={ref}>
+        <div css={avatarContainerStyle}>
+          <Avatar src={userInfo.avatar} shape="circle" size={32} />
+        </div>
+        <div css={nickNameAndEmailContainerStyle}>
+          <span css={nicknameStyle}>{userInfo.nickname}</span>
+          <span css={emailStyle}>{userInfo.email}</span>
+        </div>
       </div>
-      <div css={nickNameAndEmailContainerStyle}>
-        <span css={nicknameStyle}>{userInfo.nickname}</span>
-        <span css={emailStyle}>{userInfo.email}</span>
-      </div>
-    </div>
+    </Dropdown>
   )
-}
+})
+
+UserInfoContent.displayName = "UserInfoContent"
 
 export default UserInfoContent
