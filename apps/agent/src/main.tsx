03e46deb35c1ada7ce93fbf6542740af68c4934c
AruSeito <41471697+AruSeito@users.noreply.github.com>
2024-03-28 14:45:36 +0800

    feat: ✨ add bugsnag

--- apps/agent/src/main.tsx
+++ apps/agent/src/main.tsx
@@ -1,5 +1,8 @@
+import Bugsnag from "@bugsnag/js"
+import BugsnagPluginReact, { BugsnagErrorBoundary } from "@bugsnag/plugin-react"
 import { LicenseInfo } from "@mui/x-data-grid-premium"
 import { StrictMode } from "react"
+import React from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 import { ILLAMixpanel } from "@illa-public/mixpanel-utils"
@@ -8,6 +11,16 @@ import "@/i18n"
 import App from "./App.tsx"
 import store from "./redux/store.ts"
 
+Bugsnag.start({
+  apiKey: "a59ca89c3b66725e12abb0be6a68fbd3",
+  plugins: [new BugsnagPluginReact()],
+})
+
+// eslint-disable-next-line react-refresh/only-export-components
+const ErrorBoundary = Bugsnag.getPlugin("react")?.createErrorBoundary(
+  React,
+) as BugsnagErrorBoundary
+
 if (import.meta.env.ILLA_MUI_LICENSE) {
   LicenseInfo.setLicenseKey(import.meta.env.ILLA_MUI_LICENSE)
 }
@@ -15,9 +28,11 @@ if (import.meta.env.ILLA_MUI_LICENSE) {
 ILLAMixpanel.setDeviceID()
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
+  <ErrorBoundary>
+    <StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </StrictMode>
+  </ErrorBoundary>,
 )
