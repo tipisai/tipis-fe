29ad18e9bd7320d8b749205650ef1f8b851b2118
AruSeito <41471697+AruSeito@users.noreply.github.com>
2024-04-01 20:16:58 +0800

    feat: ✨ update vite-env.d.ts

--- apps/agent/src/vite-env.d.ts
+++ apps/agent/src/vite-env.d.ts
@@ -1,13 +1,13 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly ILLA_API_BASE_URL: string
+  readonly ILLA_CLOUD_URL: string
   readonly ILLA_MARKET_URL: string
   readonly ILLA_INSTANCE_ID: string
-  readonly ILLA_SENTRY_ENV: string
-  readonly ILLA_SENTRY_SERVER_API: string
   readonly ILLA_APP_VERSION: string
   readonly ILLA_APP_ENV: string
-  readonly ILLA_CLOUD_URL: string
+  readonly ILLA_MUI_LICENSE: string
+  readonly ILLA_POSTHOG_KEY: string
   readonly ILLA_INTERCOM_APP_ID: string
 }
 
