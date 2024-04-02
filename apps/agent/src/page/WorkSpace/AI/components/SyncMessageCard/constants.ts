736da96555cf80c7247618febffb70181051e866
Scenery <1507337624@qq.com>
2024-04-02 16:41:13 +0800

    feat: update message request diff code type

---
+++ apps/agent/src/page/WorkSpace/AI/components/SyncMessageCard/constants.ts
@@ -0,0 +1,5 @@
+export enum RUN_REQUEST_TYPE {
+  _SYS_RUN_PYTHON_CODE = "_sys_run_python_code",
+  _SYS_READ_FILE = "_sys_read_file",
+  _SYS_GET_CURRENT_TIME_IN_UTC = "_sys_get_current_time_in_utc",
+}
--- apps/agent/src/page/WorkSpace/AI/components/SyncMessageCard/index.tsx
+++ apps/agent/src/page/WorkSpace/AI/components/SyncMessageCard/index.tsx
@@ -2,7 +2,10 @@ import Icon from "@ant-design/icons"
 import { motion } from "framer-motion"
 import { FC, useState } from "react"
 import { DownIcon, UpIcon } from "@illa-public/icon"
+import LottieItem from "@/components/LottieItem"
+import tipiRunLoading from "@/config/lottieConfig/tipiRunLoading.json"
 import MarkdownMessage from "../MarkdownMessage"
+import { RUN_REQUEST_TYPE } from "./constants"
 import {
   PureMessageProps,
   SyncMessageCardProps,
@@ -12,12 +15,14 @@ import {
   actionIconStyle,
   containerStyle,
   headerContainerStyle,
+  iconStyle,
   infoContainerStyle,
   infoDescStyle,
   infoTextContainerStyle,
   infoTitleStyle,
   lineContainerStyle,
   lineStyle,
+  lottieLoadingStyle,
   messageCardAnimation,
   messageContainerStyle,
   pureMessageContainerStyle,
@@ -53,18 +58,25 @@ export const SyncMessageCard: FC<SyncMessageCardProps> = ({
   messageStatus,
 }) => {
   const [showMessage, setShowMessage] = useState(false)
-  let formatMessage
+  let formatMessage, runRequestType: RUN_REQUEST_TYPE | undefined
   try {
     const res = JSON.parse(message)
+    const functionName = res?.["function_name"]
     const code = res?.["function_arguments"]?.["code"]
     if (code) {
       formatMessage = `\`\`\`python\n${code}\n\`\`\``
+    }
+    if (functionName) {
+      runRequestType = functionName
     }
   } catch (e) {}
 
   const getInfoByStatus = useGetInfoByStatus()
 
-  const { InfoIcon, InfoTitle, infoDesc } = getInfoByStatus(messageStatus)
+  const { InfoIcon, InfoTitle, infoDesc } = getInfoByStatus(
+    messageStatus,
+    runRequestType,
+  )
 
   return (
     <div css={containerStyle}>
@@ -73,18 +85,26 @@ export const SyncMessageCard: FC<SyncMessageCardProps> = ({
         onClick={() => setShowMessage(!showMessage)}
       >
         <div css={infoContainerStyle}>
-          <div css={textAndIconContainerStyle}>
-            {InfoIcon}
-            <div css={infoTextContainerStyle}>
-              <span css={infoTitleStyle}>{InfoTitle}</span>
-              <span css={infoDescStyle(messageStatus)}>{infoDesc}</span>
+          {runRequestType ? (
+            <>
+              <div css={textAndIconContainerStyle}>
+                <div css={iconStyle(messageStatus)}>{InfoIcon}</div>
+                <div css={infoTextContainerStyle}>
+                  <span css={infoTitleStyle}>{InfoTitle}</span>
+                  <span css={infoDescStyle(messageStatus)}>{infoDesc}</span>
+                </div>
+              </div>
+              {formatMessage && (
+                <Icon
+                  component={showMessage ? UpIcon : DownIcon}
+                  css={actionIconStyle}
+                />
+              )}
+            </>
+          ) : (
+            <div css={lottieLoadingStyle}>
+              <LottieItem configJson={tipiRunLoading} autoplay loop />
             </div>
-          </div>
-          {formatMessage && (
-            <Icon
-              component={showMessage ? UpIcon : DownIcon}
-              css={actionIconStyle}
-            />
           )}
         </div>
       </div>
