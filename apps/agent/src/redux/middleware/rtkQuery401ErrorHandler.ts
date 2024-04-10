19348a5493dd797853003847e8f54d8579c01348
AruSeito <41471697+AruSeito@users.noreply.github.com>
2024-04-10 18:22:25 +0800

    fix: reset apiData when 401

--- apps/agent/src/redux/middleware/rtkQuery401ErrorHandler.ts
+++ apps/agent/src/redux/middleware/rtkQuery401ErrorHandler.ts
@@ -1,14 +1,19 @@
 import { isRejectedWithValue } from "@reduxjs/toolkit"
 import type { Middleware, MiddlewareAPI } from "@reduxjs/toolkit"
+import { teamAPI, userAPI } from "@illa-public/user-data"
 import { removeAuthToken } from "@illa-public/utils"
 import { isFetchBaseQueryError } from "../helper"
 
 export const rtkQueryErrorLogger: Middleware =
-  (_api: MiddlewareAPI) => (next) => (action) => {
+  ({ dispatch }: MiddlewareAPI) =>
+  (next) =>
+  (action) => {
     if (isRejectedWithValue(action) && isFetchBaseQueryError(action.payload)) {
       switch (action.payload.status) {
         case 401: {
           removeAuthToken()
+          dispatch(teamAPI.util.resetApiState())
+          dispatch(userAPI.util.resetApiState())
           break
         }
         default: {
