e0dcde6256f6f682c68dcd0970305cbee1e25f2a
AruSeito <41471697+AruSeito@users.noreply.github.com>
2024-04-15 19:45:16 +0800

    feat: drag & drop tab

--- apps/agent/src/Layout/Workspace/components/TipisTab/interface.ts
+++ apps/agent/src/Layout/Workspace/components/TipisTab/interface.ts
@@ -1,9 +1,4 @@
-import { TAB_TYPE } from "@/redux/ui/recentTab/interface"
-
 export interface ITipisTabProps {
-  icon: string
-  tabType: TAB_TYPE
-  tabName: string
   tabID: string
-  cacheID: string
+  index: number
 }
--- apps/agent/src/Layout/Workspace/modules/RecentTabs/mobile/index.tsx
+++ apps/agent/src/Layout/Workspace/modules/RecentTabs/mobile/index.tsx
@@ -1,22 +1,25 @@
 import { FC } from "react"
 import { useSelector } from "react-redux"
-import { getRecentTabInfos } from "@/redux/ui/recentTab/selector"
+import { getRecentTabInfosOrder } from "@/redux/ui/recentTab/selector"
 import MobileTipisTab from "../../../components/TipisTab/mobile"
 import { recentTabsContainerStyle } from "../style"
 
 const MobileRecentTabs: FC = () => {
-  const recentTabInfos = useSelector(getRecentTabInfos)
+  const tabOrders = useSelector(getRecentTabInfosOrder)
+
+  // const dispatch = useDispatch()
+  // const onReorder = useCallback(
+  //   (newOrder: string[]) => {
+  //     console.log("newOrder", newOrder)
+  //     dispatch(recentTabActions.updateCurrentRecentTabOrderReducer(newOrder))
+  //   },
+  //   [dispatch],
+  // )
+
   return (
     <div css={recentTabsContainerStyle}>
-      {recentTabInfos.map((tabInfo) => (
-        <MobileTipisTab
-          tabID={tabInfo.tabID}
-          tabType={tabInfo.tabType}
-          tabName={tabInfo.tabName}
-          icon={tabInfo.tabIcon}
-          key={tabInfo.tabID}
-          cacheID={tabInfo.cacheID}
-        />
+      {tabOrders.map((tabID, index) => (
+        <MobileTipisTab tabID={tabID} key={tabID} index={index} />
       ))}
     </div>
   )
