e59de474462d4ab4dfc5e203041126106575dd41
AruSeito <41471697+AruSeito@users.noreply.github.com>
2024-04-15 14:45:54 +0800

    fix: 🐛 can't scroll dashboard

--- apps/agent/src/page/WorkSpace/TipisDashboard/pc/style.ts
+++ apps/agent/src/page/WorkSpace/TipisDashboard/pc/style.ts
@@ -3,7 +3,6 @@ import { css } from "@emotion/react"
 export const tabsContainerStyle = css`
   width: 100%;
   height: 100%;
-  padding: 0 24px;
   overflow: hidden;
 `
 
@@ -12,8 +11,19 @@ export const tipisPCDashboardContainerStyle = css`
   height: 100%;
   display: flex;
   flex-direction: column;
+  & .ant-tabs-nav-wrap {
+    padding: 0 24px;
+  }
   & .ant-tabs-content,
   & .ant-tabs-tabpane-active {
     height: 100%;
   }
+`
+
+export const cardListContainerStyle = css`
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+  overflow-x: hidden;
+  padding: 0 24px;
 `
