3ab8a24eddcaa84c930e43bd49ecceaa90af81fd
AruSeito <41471697+AruSeito@users.noreply.github.com>
2024-04-16 14:27:42 +0800

    feat: ✨ fix invite token

---
+++ apps/agent/src/utils/invite/hook.ts
@@ -0,0 +1,72 @@
+import { App } from "antd"
+import { useCallback } from "react"
+import { useTranslation } from "react-i18next"
+import { useNavigate } from "react-router-dom"
+import { ERROR_FLAG, isILLAAPiError } from "@illa-public/illa-net"
+import { useJoinTeamMutation } from "@illa-public/user-data"
+import { getAuthToken } from "@illa-public/utils"
+import { useNavigateTargetWorkspace } from "../routeHelper/hook"
+import { setLocalTeamIdentifier } from "../storage/cacheTeam"
+
+export const useAcceptInvite = () => {
+  const [joinTeamMutation] = useJoinTeamMutation()
+  const { message } = App.useApp()
+  const { t } = useTranslation()
+  const navigate = useNavigate()
+  const navigateWorkspace = useNavigateTargetWorkspace()
+
+  const acceptInvite = useCallback(
+    async (inviteToken: string, redirectURL: string | undefined | null) => {
+      try {
+        const teamInfo = await joinTeamMutation(inviteToken).unwrap()
+        message.success({ content: t("homepage.message.join_suc") })
+        setLocalTeamIdentifier(teamInfo.identifier)
+        if (redirectURL) {
+          let parseRedirectURL = new URL(decodeURIComponent(redirectURL))
+          parseRedirectURL.searchParams.set("token", getAuthToken()!)
+          window.location.href = parseRedirectURL.toString()
+        } else {
+          navigate(`/workspace/${teamInfo.identifier}`)
+        }
+      } catch (e) {
+        if (isILLAAPiError(e)) {
+          switch (e.data.errorFlag) {
+            case ERROR_FLAG.ERROR_FLAG_INVITE_EMAIL_MISMATCH: {
+              message.error({
+                content: t("homepage.message.join_failed"),
+              })
+              break
+            }
+            case ERROR_FLAG.ERROR_FLAG_USER_ALREADY_JOINED_TEAM: {
+              message.error({
+                content: t("homepage.message.have_joined"),
+              })
+              let redirectURLString = redirectURL
+              if (redirectURLString) {
+                let redirectURL = new URL(decodeURIComponent(redirectURLString))
+                redirectURL.searchParams.set("token", getAuthToken()!)
+                window.location.href = redirectURL.toString()
+                return
+              }
+              break
+            }
+            default: {
+              message.error({
+                content: t("homepage.message.join_failed_other_error"),
+              })
+            }
+          }
+          navigateWorkspace()
+        } else {
+          navigateWorkspace()
+          message.error({
+            content: t("homepage.message.join_failed_other_error"),
+          })
+        }
+      }
+    },
+    [joinTeamMutation, message, navigate, navigateWorkspace, t],
+  )
+
+  return acceptInvite
+}
