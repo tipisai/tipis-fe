44ed7a48cfb3558eb4827684e93785c3b33bca30
Scenery <1507337624@qq.com>
2024-04-16 14:20:29 +0800

    style: update preview chat style

--- apps/agent/src/page/WorkSpace/AI/AIAgentRun/AIAgentRunPC/style.ts
+++ apps/agent/src/page/WorkSpace/AI/AIAgentRun/AIAgentRunPC/style.ts
@@ -1,9 +1,12 @@
 import { css } from "@emotion/react"
+import { getColor } from "@illa-public/color-scheme"
 
 export const rightPanelContainerStyle = css`
   height: 100%;
   display: flex;
+  align-items: center;
   overflow-y: auto;
   flex-grow: 1;
   flex-direction: column;
+  border-top: 1px solid ${getColor("grayBlue", "08")};
 `
