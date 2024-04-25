37abc89ac57cc41dcccafcd3e054bc21252a41a5
Scenery <1507337624@qq.com>
2024-04-25 15:28:56 +0800

    chore: remove desc generate

--- apps/agent/src/page/WorkSpace/AI/AIAgent/components/DescriptionEditor/index.tsx
+++ apps/agent/src/page/WorkSpace/AI/AIAgent/components/DescriptionEditor/index.tsx
@@ -1,30 +1,16 @@
-import Icon from "@ant-design/icons"
-import { App, Input } from "antd"
-import { FC, memo, useState } from "react"
+import { Input } from "antd"
+import { FC, memo } from "react"
 import { Controller, useFormContext, useFormState } from "react-hook-form"
 import { useTranslation } from "react-i18next"
-import { useSelector } from "react-redux"
-import { TipisTrack } from "@illa-public/track-utils"
-import { handleCreditPurchaseError } from "@illa-public/upgrade-modal"
-import { getCurrentTeamInfo } from "@illa-public/user-data"
 import LayoutBlock from "@/Layout/Form/LayoutBlock"
-import AIIcon from "@/assets/agent/ai.svg?react"
-import { useGeneratePromptDescriptionMutation } from "@/redux/services/agentAPI"
-import AILoadingIcon from "../../../components/AILoading/aiLoading.svg?react"
 import { IAgentForm, SCROLL_ID } from "../../interface"
-import { descContainerStyle, descTextStyle } from "./style"
 
 const DescriptionEditor: FC = memo(() => {
   const { t } = useTranslation()
-  const { control, getValues } = useFormContext<IAgentForm>()
-  const { message: messageApi } = App.useApp()
-  const [generatePromptDescription] = useGeneratePromptDescriptionMutation()
-  const [generateDescLoading, setGenerateDescLoading] = useState(false)
+  const { control } = useFormContext<IAgentForm>()
   const { errors } = useFormState({
     control: control,
   })
-
-  const currentTeamInfo = useSelector(getCurrentTeamInfo)!
 
   return (
     <Controller
@@ -42,63 +28,8 @@ const DescriptionEditor: FC = memo(() => {
         <LayoutBlock
           scrollId={SCROLL_ID.DESCRIPTION}
           title={t("editor.ai-agent.label.desc")}
-          subtitleTips={t("editor.ai-agent.generate-desc.tooltips")}
           required
           errorMessage={errors.description?.message}
-          subtitle={
-            <div
-              css={descContainerStyle}
-              onClick={async () => {
-                TipisTrack.track("click_generate_desc", {
-                  parameter1: getValues("aiAgentID") ? "edit" : "create",
-                })
-                const prompt = getValues("prompt")
-                if (!prompt) {
-                  messageApi.error({
-                    content: t("editor.ai-agent.generate-desc.blank"),
-                  })
-                  return
-                }
-                setGenerateDescLoading(true)
-                try {
-                  const desc = await generatePromptDescription({
-                    teamID: currentTeamInfo.id,
-                    prompt: prompt,
-                  }).unwrap()
-
-                  field.onChange(desc.payload)
-                } catch (e) {
-                  const isCreditError = handleCreditPurchaseError(e)
-                  !isCreditError &&
-                    messageApi.error({
-                      content: t("editor.ai-agent.generate-desc.failed"),
-                    })
-                } finally {
-                  setGenerateDescLoading(false)
-                }
-              }}
-            >
-              {generateDescLoading ? (
-                <Icon
-                  component={AILoadingIcon}
-                  spin
-                  style={{
-                    fontSize: "12px",
-                  }}
-                />
-              ) : (
-                <Icon
-                  component={AIIcon}
-                  style={{
-                    fontSize: "12px",
-                  }}
-                />
-              )}
-              <div css={descTextStyle}>
-                {t("editor.ai-agent.generate-desc.button")}
-              </div>
-            </div>
-          }
         >
           <Input.TextArea
             {...field}
