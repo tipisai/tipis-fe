c797c8c9efc6cb5bbc1bc6bba345af06888f5ad8
Scenery <1507337624@qq.com>
2024-04-28 16:07:24 +0800

    feat: optimize markdown table style

--- apps/agent/src/page/WorkSpace/AI/components/MarkdownMessage/style.ts
+++ apps/agent/src/page/WorkSpace/AI/components/MarkdownMessage/style.ts
@@ -19,10 +19,46 @@ export const markdownMessageStyle = css`
 
 export const cellStyle = css`
   min-width: 100px;
+  color: ${getColor("grayBlue", "02")};
+  border-bottom: none;
+  padding: 10px 8px;
+  border-right: 1px solid ${getColor("grayBlue", "08")};
+  &:last-of-type {
+    border-right: none;
+  }
+`
+
+export const rowStyle = css`
+  border-bottom: 1px solid ${getColor("grayBlue", "08")};
+  &:last-of-type {
+    border-bottom: none;
+  }
+`
+
+export const headStyle = css`
+  border-bottom: 1px solid ${getColor("grayBlue", "08")};
+`
+
+export const thStyle = css`
+  color: ${getColor("grayBlue", "02")};
+  background-color: #f8f9fa;
+  padding: 13px 8px;
+  border-bottom: none;
+  text-align: start;
+  border-right: 1px solid ${getColor("grayBlue", "08")};
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  &:last-of-type {
+    border-right: none;
+  }
 `
 
 export const tableStyle = css`
   margin: 8px 0;
+  box-shadow: unset;
+  border-radius: 8px;
+  border: 1px solid ${getColor("grayBlue", "08")};
 `
 
 export const listStyle = css`
