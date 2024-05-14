defe3e0822962c43024317daf7f07ca6eb8cffb8
Scenery <1507337624@qq.com>
2024-05-14 14:08:12 +0800

    feat: get auth info by server req

--- apps/agent/src/page/UserAuth/AuthPage/index.tsx
+++ apps/agent/src/page/UserAuth/AuthPage/index.tsx
@@ -1,5 +1,7 @@
+// import { Button } from "antd"
 import { FC, useState } from "react"
 import { FormProvider, useForm } from "react-hook-form"
+// import { useLazyGetProvidersQuery } from "@illa-public/user-data"
 import InputPage from "./components/InputPage"
 import ResendPage from "./components/ResendPage"
 import { AUTH_STEP, IEmailForm } from "./interface"
@@ -8,9 +10,17 @@ import { containerStyle } from "./style"
 const AuthPage: FC = () => {
   const [authStep, setAuthStep] = useState<AUTH_STEP>(AUTH_STEP.INPUT_PAGE)
   const formProps = useForm<IEmailForm>()
+  // const [trigger] = useLazyGetProvidersQuery()
   return (
     <FormProvider {...formProps}>
       <div css={containerStyle}>
+        {/* <Button
+          onClick={() => {
+            trigger(null)
+          }}
+        >
+          test
+        </Button> */}
         {authStep === AUTH_STEP.INPUT_PAGE ? (
           <InputPage setAuthStep={setAuthStep} />
         ) : (
